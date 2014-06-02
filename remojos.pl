212a8acfa88f82af42a82310de68029ddfb79eac
Marc Egea i Sala <marc.egea.sala@gmail.com>
2014-06-02 06:54:53 +0200

    Generate active & passive sessions.

--- remojos.pl
+++ remojos.pl
@@ -4,20 +4,68 @@ use v5.10;
 use strict;
 use Mojolicious::Lite;
 use File::Basename;
+use String::Random qw/random_string/;
 
 get_args(@_);
 
+my %active_sessions;
+my %passive_sessions;
+
+# Initial route
 get '/' => sub { shift->render( text => 'todo' ) };
+# Passive
+get '/p'      => sub { create_passive(shift) };
+get '/p/:sid' => sub { open_passive(shift) };
+# Active
+get '/a/:sid' => sub { open_active(shift) };
 
 sub get_args {
     my $path = shift @ARGV;
     my $name = basename($path);
     my $dir  = dirname($path);
 
-    say $path;
-    say $name || 'no-name';
-    say $dir || 'no-dir';
     push @{app->static->paths}, $dir;
+}
+
+sub create_passive {
+    my $self = shift;
+
+    my $sid = random_string('ccccc');
+
+    $sid = random_string('cccc') while $passive_sessions{$sid};
+
+    $self->redirect_to("/p/$sid");
+}
+
+sub open_passive {
+    my $self = shift;
+    my $sid  = $self->stash('sid');
+
+    if ($passive_sessions{$sid}) {
+        $self->render( text => 'Busy' );
+    }
+    else {
+        $passive_sessions{$sid} = 1;
+        $self->render( text => "Sessió $sid" );
+    }
+}
+
+sub open_active {
+    my $self = shift;
+    my $sid  = $self->stash('sid');
+
+    if ($passive_sessions{$sid}) {
+        if ($active_sessions{$sid}) {
+            $self->render( text => 'Busy' );
+        }
+        else {
+            $active_sessions{$sid} = 1;
+            $self->render( text => "Sessió $sid" );
+        }
+    }
+    else {
+        $self->render( text => 'Not available'' );
+    }
 }
 
 app->start('daemon');
